541cd19d148c7f37d9887468671bdd126e697a5b
Antti Jankeri <antti.jankeri@gmail.com>
2023-11-27 16:31:12 +0100

    feat: saving files

--- src/routes/images.ts
+++ src/routes/images.ts
@@ -1,4 +1,7 @@
+import { randomUUID } from "crypto";
 import express from "express";
+import path from "path";
+import { writeFile } from "fs/promises";
 
 const router = express.Router();
 
@@ -6,8 +9,18 @@ router.get("/:id", async (_req, res) => {
   res.send("COOL GET");
 });
 
-router.post("/", async (_req, res) => {
-  res.send("COOL POST");
+router.post("/", async (req, res) => {
+  try {
+    const file = req.body as Buffer;
+
+    const filePath = path.resolve(path.join("..", "..", "data", randomUUID()));
+
+    await writeFile(filePath, file, { encoding: "binary" });
+
+    res.json({ filePath });
+  } catch (error) {
+    res.status(500).send("Failed to save file");
+  }
 });
 
 router.delete("/:id", async (_req, res) => {
